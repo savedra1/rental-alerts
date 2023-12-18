7f60faa50be8d8b030066a3631c35dfc7d9ec391
savedram <michaelsavedra@gmail.com>
2023-12-18 16:43:24 +0000

    Updated source paths.

--- aws-infra/lambda/lambda_function.tf
+++ aws-infra/lambda/lambda_function.tf
@@ -1,14 +1,15 @@
 # main lambda function config
 
 resource "aws_lambda_function" "rental_alerts" {
-  function_name    = "rental-alerts"
-  role             = aws_iam_role.lambda_exec_role2.arn
-  handler          = "lambda_function.lambda_handler"
-  runtime          = "python3.10"
-  timeout          = 900
-  memory_size      = 1000
-  layers           = [aws_lambda_layer_version.lambda_layer.arn, aws_lambda_layer_version.webdriver_layer.arn]
-  filename         = "function_code.zip"
-  source_code_hash = filebase64sha256("function_code.zip")
+  function_name                  = "rental-alerts"
+  role                           = aws_iam_role.lambda_exec_role2.arn
+  handler                        = "lambda_function.lambda_handler"
+  runtime                        = "python3.10"
+  timeout                        = 900
+  memory_size                    = 1000
+  reserved_concurrent_executions = 1
+  layers                         = [aws_lambda_layer_version.lambda_layer.arn, aws_lambda_layer_version.webdriver_layer.arn]
+  filename                       = "function_code.zip"
+  source_code_hash               = filebase64sha256("function_code.zip")
 }
 
