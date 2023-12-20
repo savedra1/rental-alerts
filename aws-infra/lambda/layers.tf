2646753cca0c9cd5ea2e46cf3cc0c045af023f13
savedram <michaelsavedra@gmail.com>
2023-12-20 19:21:21 +0000

    rebuild

--- aws-infra/lambda/layers.tf
+++ aws-infra/lambda/layers.tf
@@ -3,7 +3,7 @@
 resource "aws_lambda_layer_version" "main_layer" {
   filename            = "python.zip"
   layer_name          = "rentalAlerts"
-  compatible_runtimes = ["python3.10"]
+  compatible_runtimes = ["python3.12"]
   source_code_hash = filebase64sha256("python.zip") # Allow update each time TF runs
 }
 
