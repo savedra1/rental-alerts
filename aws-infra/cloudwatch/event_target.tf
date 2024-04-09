d1c271a4e1efe84f4bee197c60e0682d03943fc9
savedram <michael.savedra@ovo.com>
2024-04-09 14:32:30 +0100

    switching back on

--- aws-infra/cloudwatch/event_target.tf
+++ aws-infra/cloudwatch/event_target.tf
@@ -1,7 +1,7 @@
 # Define teh target of the clpudwatch event rule
 
 # Commenting to disable
-#resource "aws_cloudwatch_event_target" "lambda_target" {
-#  rule = aws_cloudwatch_event_rule.lambda_trigger.name
-#  arn  = var.func_arn
-#}
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule = aws_cloudwatch_event_rule.lambda_trigger.name
+  arn  = var.func_arn
+}
