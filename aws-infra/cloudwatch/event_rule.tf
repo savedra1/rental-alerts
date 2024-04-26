353f2e5c50b40217693da1cd0f3ff72543d9e116
savedram <michael.savedra@ovo.com>
2024-04-26 22:08:16 +0100

    destroy cron

--- aws-infra/cloudwatch/event_rule.tf
+++ aws-infra/cloudwatch/event_rule.tf
@@ -1,8 +1,8 @@
 # Cron schedule event definition
 
 
-#resource "aws_cloudwatch_event_rule" "lambda_trigger" {
-#  name        = "renal-alert-rule"
-#  description = "Trigger Lambda on a schedule"
-#  schedule_expression = "cron(0 5-21 * * ? *)"
-#}
+resource "aws_cloudwatch_event_rule" "lambda_trigger" {
+  name        = "renal-alert-rule"
+  description = "Trigger Lambda on a schedule"
+  schedule_expression = "cron(0 5-21 * * ? *)"
+}
