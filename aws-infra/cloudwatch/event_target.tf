19e24589028534ba9056a48e3a0b7d5fff6fbb40
Michael Savedra <99875823+savedra1@users.noreply.github.com>
2024-05-05 10:19:12 +0100

    Bug/fix (#30)
    * added bool option to cloudfront module
    
    * added bool option to cloudfront module

--- aws-infra/cloudwatch/event_target.tf
+++ aws-infra/cloudwatch/event_target.tf
@@ -2,6 +2,7 @@
 
 #Commenting to disable
 resource "aws_cloudwatch_event_target" "lambda_target" {
+  count = var.active ? 1 : 0
   rule = aws_cloudwatch_event_rule.lambda_trigger.name
   arn  = var.func_arn
 }
